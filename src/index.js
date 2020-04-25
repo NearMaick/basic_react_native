205a7eb2d8cf12ddab543a8042ddf084232931d0
Maick Souza <maick_a_s@msn.com>
2020-04-25 11:20:00 -0300

    basic04

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
 
 import api from './services/api';
 
@@ -13,6 +13,17 @@ export default function App() {
     });
   }, []);
 
+  async function handleAddProject() {
+    const response = await api.post('/projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Maick Souza',
+    });
+
+    const project = response.data;
+
+    setProjects([ ...projects, project ]);
+  }
+
   return(
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
@@ -25,6 +36,14 @@ export default function App() {
             <Text style={styles.project}>{project.title}</Text>
           )}
         />
+
+        <TouchableOpacity 
+          activeOpacity={0.7} 
+          style={styles.button} 
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
 
     </>
@@ -41,5 +60,19 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 30,
     fontWeight: 'bold',
+  },
+
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16,
   }
 });
